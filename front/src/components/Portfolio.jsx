54dc82676defd3d04ab4b25cadf8d4d8039aec42
tariq-khan9 <tariqkhan.cs9@gmail.com>
2023-05-17 13:37:23 +0100

    first commit

---
+++ front/src/components/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './sass/portfolio.scss'
+
+export default function Portfolio() {
+  return (
+    <div className='portfolio'>
+      <h1>hello world</h1>
+    </div>
+  )
+}
